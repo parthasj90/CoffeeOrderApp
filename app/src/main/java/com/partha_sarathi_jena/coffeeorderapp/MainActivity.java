bf04748e143b21ddafa9bd24bab0e65b0307f170
parthasj1990 <jena.p@husky.neu.edu>
2017-01-29 15:06:16 -0500

    UI changes and implemented email functionality

--- app/src/main/java/com/partha_sarathi_jena/coffeeorderapp/MainActivity.java
+++ app/src/main/java/com/partha_sarathi_jena/coffeeorderapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.partha_sarathi_jena.coffeeorderapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -28,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
         hasChocolate = cb_choc.isChecked();
         String message = createOrderSummary();
         displayMessage(message);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"parthasj90@gmail.com"});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Coffee Order for: " + name);
+        intent.putExtra(Intent.EXTRA_TEXT   , message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(Intent.createChooser(intent, "Send mail..."));
+        }
     }
 
     private String createOrderSummary()
